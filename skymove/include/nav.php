ca724540a2b60da80457b8a46db37e2b8254129d
gunnercs <53595134+gunnercs@users.noreply.github.com>
2022-06-22 10:50:56 +0700

    skymove

---
+++ skymove/include/nav.php
@@ -0,0 +1,49 @@
+
+<nav class="navbar navbar-expand-lg fixed-top trans-navigation">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+            <h1 id="logoband">SKY <span>MOVE</span></h1>
+            <!-- <img src="logo/logo.png" alt="SKY MOVE" class="img-fluid b-logo lg130"> -->
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-bars"></i>
+            </span>
+          </button>
+        <div class="collapse navbar-collapse justify-content-end" id="mainNav">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="about.html">หน้าหลัก</a>
+                </li>
+               <!-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarWelcome" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        หน้าหลัก
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarWelcome">
+                        <a class="dropdown-item " href="index.html">
+                            Home-1
+                        </a>
+                        <a class="dropdown-item " href="index-2.html">
+                            Home-2
+                        </a> 
+                        <a class="dropdown-item " href="index-3.html" target="blank">
+                            Onepage
+                        </a>
+                    </div>
+                </li> -->
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="about.html">บริการ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="service.html">ผลงาน</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="service.html">การรับรอง</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="contact.html">ติดต่อเรา</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ skymove/index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<?php include 'include/header.php';?>
+<body data-spy="scroll" data-target=".fixed-top">
+<style>
+    .lg130{
+        width: 130px;
+        max-width: 100%;
+    }
+</style>
+
+<?php include 'include/nav.php';?>
+
+    <!--MAIN HEADER AREA END -->
+
+<!--MAIN BANNER AREA START -->
+<div class="banner-area banner-3">
+    <div class="overlay dark-overlay"></div>
+    <div class="d-table">
+        <div class="d-table-cell">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 m-auto text-center col-sm-12 col-md-12">
+                        <div class="banner-content content-padding">
+                            <h5 class="subtitle">บริการขนย้ายทุกประเภท ทั่วไทย ในราคาย่อมเยาว์</h5>
+                            <h1 class="banner-title">สะดวก ประหยัด และมีความปลอดภัยสูง</h1>
+                            <p>รับจ้างขนย้ายราคาถูก ผู้นำด้านบริการรับจ้างขนย้ายบ้าน และรับจ้างขนย้ายทุกประเภท บริการตลอด 24 ชม. ประเมินค่าขนย้ายฟรี! ปรึกษาเราก่อน คุยง่ายเป็นกันเอง</p>
+
+                            <a href="#" class="btn btn-white btn-circled">เพิ่มเติม</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--MAIN HEADER AREA END -->
+<!--  ABOUT AREA START  -->
+    <section id="intro" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 col-md-12">
+                    <div class="section-heading">
+                        <p class="lead">บริการขนย้ายบ้าน ย้ายสำนักงาน ย้ายคอนโด ย้ายตู้เชฟ บริการย้ายของ บริการขนย้ายเฟอร์นิเจอร์ บริการขนย้ายคอมพิวเตอร์ ย้ายเซิร์ฟเวอร์ บริการขนย้ายเปียโน บริการขนย้ายทุกประเภท บริการถอด-ประกอบสินค้า </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- <div class="col-lg-5  d-none d-lg-block col-sm-12">
+                    <div class="intro-img">
+                        <img src="images/banner/why-choose.png" alt="intro-img" class="img-fluid">
+                    </div>
+                </div> -->
+                <div class="col-lg-12 col-md-12 col-sm-12 ">
+                    <div class="row">
+                        <div class="col-lg-6 col-sm-6 col-md-6">
+                        <div class="service-box">
+                            <div class="service-img-icon">
+                                <img src="https://img.icons8.com/external-flat-wichaiwi/100/000000/external-convenient-generation-z-flat-wichaiwi.png" style="width: 64px;"/>
+                            </div>
+                            <div class="service-inner">
+                                <h4>สะดวก</h4>
+                                <p>คุณไม่ต้องทำอะไร เรามีพนักงานจัดการให้หมด</p>
+                            </div>
+                        </div>
+                            
+                        </div>
+                        <div class="col-lg-6 col-sm-6 col-md-6">
+                            <div class="service-box">
+                                <div class="service-img-icon">
+                                    <img src="https://img.icons8.com/external-nawicon-flat-nawicon/100/000000/external-save-money-finance-nawicon-flat-nawicon.png" style="width: 64px;"/>
+                                </div>
+                                <div class="service-inner">
+                                    <h4>ประหยัด</h4>
+                                    <p>ราคาไม่แพง ทุกคนเข้าถึงได้</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="col-lg-6 col-sm-6 col-md-6">
+                            <div class="service-box">
+                                <div class="service-img-icon">
+                                    <img src="https://img.icons8.com/color/100/000000/safe-ok.png" style="width: 64px;"/>
+                                </div>
+                                <div class="service-inner">
+                                    <h4>ปลอดภัย</h4>
+                                    <p>เรามีบริการแพ็คสินค้าอย่างดี ป้องกันทรัพย์สินของคุณอย่างดี</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6 col-md-6">
+                            <div class="service-box">
+                                <div class="service-img-icon">
+                                    <img src="https://img.icons8.com/external-tulpahn-outline-color-tulpahn/100/000000/external-thailand-digital-nomad-tulpahn-outline-color-tulpahn.png" style="width: 64px;"/>
+                                </div>
+                                <div class="service-inner">
+                                    <h4>ทั่วไทย</h4>
+                                    <p>ไม่ว่าคุณจะอยู่ที่ไหน เรียกใช้เรา เราจะไปหาคุณ</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--  ABOUT AREA END  -->
+
+<!--  SERVICE AREA START  -->
+<section id="about" class="bg-light">
+    <div class="about-bg-img d-none d-lg-block d-md-block"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-sm-12 col-md-8">
+                <div class="about-content">
+                    <h2 class="subtitle">SKY MOVE</h2>
+                    <h3>บริการ 24 ชั่วโมง ไม่มีวันหยุด</h3>
+                    
+
+                    <ul class="about-list">
+                        <li><i class="icofont icofont-check-circled"></i> สะดวก</li>
+
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> ประหยัด
+                        </li>
+
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> เข้าถึงได้
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> บริการ 24 ชั่วโมง
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> ปลอดภัย
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--  SERVICE AREA END  -->
+
+<!--  SERVICE PARTNER START  -->
+<section id="service-head" class=" bg-feature" style="padding-bottom: 0px;">
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 m-auto">
+                <div class="section-heading text-white">
+                    <h4 class="section-title">ประเมินราคา</h4>
+                    <p>เลือกเมนูข้างล่างเพื่อประเมินราคา</p>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</section>
+    <section id="pricing" class="section-padding bg-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="pricing-block " style="background-color: #fff;">
+                        <div class="price-header">
+                            <i class="icofont-motor-bike"></i>
+
+                            <h4 class="price" >S</h4>
+                        </div>
+                        <div class="line"></div>
+                        <ul>
+                            <li>ขนย้ายขนาดเล็ก</li>
+                        </ul>
+
+                        <a href="#" class="btn btn-hero btn-circled">เลือก</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="pricing-block " style="background-color: #fff;">
+                        <div class="price-header">
+                            <i class="icofont-car-alt-2"></i>
+
+                            <h4 class="price">M</h4>
+                        </div>
+                        <div class="line"></div>
+                        <ul>
+                            <li>ขนย้ายขนาดกลาง</li>
+                        </ul>
+
+                        <a href="#" class="btn btn-hero btn-circled">เลือก</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="pricing-block" style="background-color: #fff;">
+                        <div class="price-header">
+                            <i class="icofont-truck-alt"></i>
+
+                            <h4 class="price">L</h4>
+                        </div>
+                        <div class="line"></div>
+                        <ul>
+                            <li>ขนย้ายขนาดใหญ่</li>
+                        </ul>
+
+                        <a href="#" class="btn btn-hero btn-circled">เลือก</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!--  FOOTER AREA START  -->
+<section id="footer" class="section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-sm-8 col-md-8">
+                <div class="footer-widget footer-link">
+                    <h4>SKY MOVE<br> สกายมูฟ</h4>
+                    <p>บริการขนย้ายบ้าน ย้ายสำนักงาน ย้ายคอนโด ย้ายตู้เชฟ บริการย้ายของ บริการขนย้ายเฟอร์นิเจอร์ บริการขนย้ายคอมพิวเตอร์ ย้ายเซิร์ฟเวอร์ บริการขนย้ายเปียโน บริการขนย้ายทุกประเภท บริการถอด-ประกอบสินค้า</p>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4 col-md-4">
+                <div class="footer-widget footer-link">
+                    <h4>บริการ</h4>
+                    <ul>
+                        <li><a href="#">บริการ</a></li>
+                        <li><a href="#">ผลงาน</a></li>
+                        <li><a href="#">การรับรอง</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            
+            <div class="col-lg-3 col-sm-6 col-md-6">
+                <div class="footer-widget footer-text">
+                    <h4>ติดต่อเรา</h4>
+                    <p class="mail"><span>E-Mail:</span> skymove2019@gmail.com</p>
+                    <p><span>โทร :</span>0654165898</p>
+                    <p><span>ที่อยู่:</span> 999/109 หมู่บ้าน ศรีเจริญวิลล่า หมู่ที่ 6 ต.เทพารักษ์ อ.เมืองสมุทรปราการ จ.สมุทรปราการ 10270</p>
+                </div>
+            </div>
+            
+        </div>
+        <div id="fixxph">
+            <span id="fixxph0" onclick="window.location.href = 'tel:+66654165898'"><i class="icofont-phone" style="font-size: 2rem;color: #f2f2f2;"></i></span>
+            <span id="fixxph1" onclick="window.location.href = 'https://www.facebook.com/SKY-MOVE-105849608837424'"><i class="icofont-facebook" style="font-size: 2rem;color: #1b74e4;"></i></span>
+            <span id="fixxph2" onclick="window.location.href = 'https://page.line.me/304tjazs'"><i class="icofont-line" style="font-size: 2rem;color: #06c755;"></i></span>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="footer-copy">
+                    © 2019 SKY MOVE LTD.
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include 'include/foot.php';?>
+
+
+  </body>
+  </html>
+   
